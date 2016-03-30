33c07aba0e9840b9f024a5c97c78bbe12f955b66
sco1 <sco1.git@gmail.com>
2016-03-30 12:23:27 -0400

    Add inline help, version restriction, minor tweaks

--- mlapp2classdef.m
+++ mlapp2classdef.m
@@ -1,4 +1,23 @@
 function mlapp2classdef()
+% MLAPP2CLASSDEF() prompts the user to select an App Designer GUI, packaged
+% as an *.mlapp file, and converts the GUI's class definition from an XML 
+% file to a standalone *.m file.
+%
+% The class definition for an App Designer GUI is embedded in an XML file 
+% located in a subfolder of the packaged *.mlapp file, which can be 
+% accessed like a *.zip file. MLAPP2CLASSDEF strips the XML header & footer
+% and saves the class definition to a *.m file located in the same path as 
+% the *.mlapp file.
+%
+% MLAPP2CLASSDEF assumes that the targeted *.mlapp file is a GUI created by
+% MATLAB's App Designer. Other packaged apps are not explicitly supported.
+
+if verlessthan('matlab', '7.9')
+    error('mlapp2classdef:UnsupportedMATLABver', ...
+          'MATLAB releases prior to R2009b are not supported' ...
+          );
+end
+
 % Unzip user selected MATLAB App, which are packaged in a renamed zip file
 [filename, pathname] = uigetfile('*.mlapp', 'Select MATLAB App');
 [~, appname] = fileparts(filename);
@@ -9,15 +28,18 @@ unzip(fullfile(pathname, filename), tmpdir);
 % Since there isn't really much XML-ness to this XML file, no need to
 % utilize a full-fledged parser. MATLAB's won't open it anyway...
 xmlfile = fullfile(tmpdir, 'matlab', 'document.xml');
-nlines = countlines(xmlfile);
-fID = fopen(xmlfile, 'r');
 
+% Get a count of lines in the xml file to preallocate the cell array in
+% memory. If no count can be made, revert to growing the array in memory
+nlines = countlines(xmlfile);
 if ~isempty(nlines)
     A = cell(nlines, 1);
 else
     A = {};
 end
 
+% Read XML file line-by-line into a cell array to make later export simpler
+fID = fopen(xmlfile, 'r');
 ii = 1;
 while ~feof(fID)
     A{ii} = fgetl(fID);
@@ -59,19 +81,19 @@ switch myOS
     case 'GLNXA64'
         % Linux systems
         warning('mlapp2classdef:UnsupportedOS', ...
-              'OS currently unsupported: ''%s''', myOS ...
+              'Line counting currently unsupported in OS ''%s''', myOS ...
               );
         nlines = [];
     case 'MACI64'
         % Mac OS systems
         warning('mlapp2classdef:UnsupportedOS', ...
-              'OS currently unsupported: ''%s''', myOS ...
+              'Line counting currently unsupported in OS ''%s''', myOS ...
               );
         nlines = [];
     otherwise
         % Unknown/unsupported OS
         warning('mlapp2classdef:UnsupportedOS', ...
-              'OS currently unsupported: ''%s''', myOS ...
+              'Line counting currently unsupported in OS ''%s''', myOS ...
               );
         nlines = [];
 end
