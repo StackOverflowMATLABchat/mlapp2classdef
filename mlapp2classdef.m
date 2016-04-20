9e537d2856d0cd818003191b3d17cd238621050f
sco1 <sco1.git@gmail.com>
2016-04-20 07:57:32 -0400

    Update inline docs, regex fix, syntax tweaks
    Update inline documentation to include the addition of user specified
    files to process. Minor tweaks to existing comments to align style.
    
    The regex to identify and remove the XML header and footer has been
    tweaked slightly to preclude invalid matches in the body of the code
    (e.g. if mlapp2classdef() is called in the GUI, mlapp2 is removed). This
    issue is also handled by limiting the regex to the first and last lines
    of the file, but the regex has also been adjusted in the event more
    generic handling is added for the future.
    
    Minor syntax updates to bring additions in line with existing code.

--- mlapp2classdef.m
+++ mlapp2classdef.m
@@ -1,12 +1,19 @@
-function mlapp2classdef(pathToMlapp)
-% MLAPP2CLASSDEF prompts the user to select an App Designer GUI, packaged
-% as an *.mlapp file, and converts the GUI's class definition from an XML 
-% file to a standalone *.m file.
+function mlapp2classdef(pathToMLapp)
+% MLAPP2CLASSDEF an App Designer GUI, packaged as an *.mlapp file, and
+% converts the GUI's class definition from an XML file to a standalone *.m 
+% file.
 %
-% The class definition for an App Designer GUI is embedded in an XML file 
-% located in a subfolder of the packaged *.mlapp file, which can be 
+% MLAPP2CLASSDEF() prompts the user to select a single *.mlapp file for
+% processing
+%
+% MLAPP2CLASSDEF(pathToMLapp) processes the files specified by the user.
+% pathToMLapp can be a string for a single file or a cell array of strings
+% for multiple files. Filepaths should be absolute.
+%
+% The class definition for an App Designer GUI is embedded in an XML file
+% located in a subfolder of the packaged *.mlapp file, which can be
 % accessed like a *.zip file. MLAPP2CLASSDEF strips the XML header & footer
-% and saves the class definition to a *.m file located in the same path as 
+% and saves the class definition to a *.m file located in the same path as
 % the *.mlapp file.
 %
 % MLAPP2CLASSDEF assumes that the targeted *.mlapp file is a GUI created by
@@ -18,27 +25,31 @@ if verLessThan('matlab', '7.9')
           );
 end
 
-% Handle file selection:
+% Choose appropriate behavior based on number of inputs
 if nargin == 0
-  [filename, pathname] = uigetfile('*.mlapp', 'Select MATLAB App');
-  [~, appname] = fileparts(filename);
-else  
-  validateattributes(pathToMlapp,{'char','cell'},{'vector'}); 
-  if iscell(pathToMlapp)
-    [pathname,appname,ext] = cellfun(@fileparts,pathToMlapp,'UniformOutput',false);  
-  else
-    [pathname,appname,ext] = fileparts(pathToMlapp);  
-  end
-  filename = strcat(appname,ext);
+    % No input selected, prompt user to select a MATLAB app to process
+    % Currently limited to single file selection
+    [filename, pathname] = uigetfile('*.mlapp', 'Select MATLAB App');
+    [~, appname] = fileparts(filename);
+else
+    % TODO: Refactor to more verbose error generation
+    validateattributes(pathToMLapp, {'char', 'cell'}, {'vector'});
+    if iscell(pathToMLapp)
+        [pathname, appname, ext] = cellfun(@fileparts, pathToMLapp, 'UniformOutput', false);
+    else
+        [pathname, appname, ext] = fileparts(pathToMLapp);
+    end
+    filename = strcat(appname, ext);
 end
 
-if iscell(pathToMlapp)
-  for indF = 1:numel(pathToMlapp)
-    processMlapp(pathname{indF}, filename{indF}, appname{indF});
-    % TODO: Add a counter of successfully converted files.
-  end
+if iscell(pathToMLapp)
+    for indF = 1:numel(pathToMLapp)
+        % TODO: Check for existence of file
+        processMlapp(pathname{indF}, filename{indF}, appname{indF});
+        % TODO: Add a counter of successfully converted files.
+    end
 else
-  processMlapp(pathname, filename, appname);
+    processMlapp(pathname, filename, appname);
 end
 
 end
@@ -72,7 +83,9 @@ end
 fclose(fID);
 
 % Strip out header & footer, then save to a *.m file
-A([1,end]) = regexprep(A([1,end]), '(^.*)(?=classdef)|(?<=end)(\].*$)', '');
+% Limit search to first & last lines of file, currently all that is
+% modified by MATLAB to wrap the class definition in XML
+A([1,end]) = regexprep(A([1,end]), '(^.*)\[(?=classdef)|(?<=end)(\].*$)', '');
 
 fID = fopen(fullfile(pathname, sprintf('%s.m', appname)), 'w');
 for ii = 1:length(A)
